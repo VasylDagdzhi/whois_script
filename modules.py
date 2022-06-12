d2ec12231d8263ae4cb8c256a82d8b3bf688d814
LJNERO8 <ljnero8@gmail.com>
2022-06-12 20:26:21 +0300

    1.0 stable

--- modules.py
+++ modules.py
@@ -3,7 +3,6 @@ import pydig
 import whois
 import validators
 import tldextract
-import tabulate
 
 
 class bcolors:
@@ -12,9 +11,11 @@ class bcolors:
     FAIL = '\033[91m'  # RED
     PURPLE = '\033[35m'  # PURPLE
     RESET = '\033[0m'  # RESET COLOR
+    WHITE = '\033[1;37m'  # WHITE COLOR
 
 
 def get_domain_statuses(whois_raw_data):
+    status = True;
     print('\033[1;32mDomain statuses:')
     for i in whois_raw_data.statuses:
         split_str = i.split(" ")
@@ -25,20 +26,24 @@ def get_domain_statuses(whois_raw_data):
         y = float(24 / x )
         if y <= 1:
             tabulator = tabulator1
-        elif 1 < y < 2:
+        elif 1 < y < 2.2:
             tabulator = tabulator2
-        elif 2 < y:
+        elif 2.2 < y:
             tabulator = tabulator3
         if "clientHold" == split_str[0]:
             print('\t', bcolors.FAIL, split_str[0], tabulator, split_str[1])
+            status = False
         elif "serverHold" == split_str[0]:
             print('\t', bcolors.FAIL, split_str[0], tabulator, split_str[1])
         elif "pendingDelete" == split_str[0]:
             print('\t', bcolors.WARNING, split_str[0], tabulator, split_str[1])
+            status = False
         elif "redemptionPeriod" == split_str[0]:
             print('\t', bcolors.PURPLE, split_str[0], tabulator, split_str[1])
+            status = False
         else:
             print('\t', bcolors.RESET, split_str[0], tabulator, split_str[1])
+        return status
 
 
 def get_data(domain):
@@ -46,18 +51,21 @@ def get_data(domain):
     if whois_raw_data is None:
         print(bcolors.FAIL, 'This domain is not registered.')
     else:
-        print('\033[1;32mDomain name:\t', bcolors.RESET, whois_raw_data.name)
-        print('\033[1;32mRegistrar:\t', bcolors.RESET, whois_raw_data.registrar)
+        print('\033[1;32mDomain name:\t', bcolors.WHITE, whois_raw_data.name)
+        print('\033[1;32mRegistrar:\t', bcolors.WHITE, whois_raw_data.registrar)
         print('\033[1;32mNameservers:')
         for i in whois_raw_data.name_servers:
-            print('\t', bcolors.RESET, i)
+            print('\t', bcolors.WHITE, i)
         creation_date = whois_raw_data.creation_date
         print('\033[1;32mRegistered at:\t', bcolors.RESET, creation_date.ctime(), '\033[1;32m\tBy Kyiv time:\t',
               bcolors.RESET, creation_date.astimezone(pytz.timezone('Europe/Kiev')))
         print('\033[1;32mUpdated at:\t', bcolors.RESET, whois_raw_data.last_updated.ctime(), '\t')
         print('\033[1;32mExpires at:\t', bcolors.RESET, whois_raw_data.expiration_date.ctime(), '\t')
-        get_domain_statuses(whois_raw_data)
-        get_dns_data(whois_raw_data)
+        status = get_domain_statuses(whois_raw_data)
+        if status:
+            get_dns_data(whois_raw_data)
+        else:
+            print(bcolors.FAIL, "This domain is not active.", bcolors.RESET)
 
 
 def get_dns_data(whois_raw_data):
@@ -67,6 +75,9 @@ def get_dns_data(whois_raw_data):
         a_query = pydig.query(str(whois_raw_data.name), 'A')
         for i in a_query:
             print('\t\033[1;32mA\t', bcolors.RESET, i)
+        mx_query = pydig.query(str(whois_raw_data.name), 'MX')
+        for i in mx_query:
+            print('\033[1;32m\tSPF\t', bcolors.WHITE, i)
         txt_query = pydig.query(str(whois_raw_data.name), 'TXT')
         for i in txt_query:
             if 'v=spf' in i:
@@ -77,7 +88,6 @@ def get_dns_data(whois_raw_data):
         for i in dkim:
             if 'v=DKIM' in i:
                 print('\033[1;32m\tDKIM\t', bcolors.RESET, i)
-
     else:
         ns1a = str(pydig.query(whois_raw_data.name_servers[0], 'A'))
         ns2a = str(pydig.query(whois_raw_data.name_servers[1], 'A'))
@@ -92,16 +102,19 @@ def get_dns_data(whois_raw_data):
         )
         query = resolver.query(str(whois_raw_data.name), 'A')
         print('\033[1;32mDNS lookup information:\t')
-        a_query = pydig.query(str(whois_raw_data.name), 'A')
+        a_query = resolver.query(str(whois_raw_data.name), 'A')
         for i in a_query:
             print('\t\033[1;32mA\t', bcolors.RESET, i)
-        txt_query = pydig.query(str(whois_raw_data.name), 'TXT')
+        mx_query = resolver.query(str(whois_raw_data.name), 'MX')
+        for i in mx_query:
+            print('\033[1;32m\tMX\t', bcolors.WHITE, i)
+        txt_query = resolver.query(str(whois_raw_data.name), 'TXT')
         for i in txt_query:
             if 'v=spf' in i:
                 print('\033[1;32m\tSPF\t', bcolors.RESET, i)
             else:
                 print('\033[1;32m\tTXT\t', bcolors.RESET, i)
-        dkim = pydig.query('default._domainkey.' + str(whois_raw_data.name), 'TXT')
+        dkim = resolver.query('default._domainkey.' + str(whois_raw_data.name), 'TXT')
         for i in dkim:
             if 'v=DKIM' in i:
                 print('\033[1;32m\tDKIM\t', bcolors.RESET, i)
